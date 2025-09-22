39289226c237b2b14fdaf2605104ac18f6d50578
PC1\admin <herokkw1@gmail.com>
2025-09-22 16:56:16 +0900

    ??

--- LogCheck/obj/Debug/net8.0-windows/ProgramsList.g.i.cs
+++ LogCheck/obj/Debug/net8.0-windows/ProgramsList.g.i.cs
@@ -9,7 +9,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using LiveChartsCore.SkiaSharpView.WPF;
 using LogCheck;
+using LogCheck.Converters;
+using LogCheck.Models;
 using MaterialDesignThemes.Wpf;
 using MaterialDesignThemes.Wpf.Converters;
 using MaterialDesignThemes.Wpf.Transitions;
@@ -115,7 +118,7 @@ namespace LogCheck {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -131,7 +134,7 @@ namespace LogCheck {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
